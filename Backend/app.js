2e6b05a2741bf989e5c02e0caa8108243006ffd5
KhanhLam00 <lam18012021@gmail.com>
2026-04-22 04:11:27 +0700

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,12 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
