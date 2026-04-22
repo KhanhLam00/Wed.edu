bc9f9c2301651aea953fd25376a0873c3a74791a
KhanhLam00 <lam18012021@gmail.com>
2026-04-22 13:30:45 +0700

    Update TeacherSubmissionDetail.jsx

--- Frontend/src/pages/Teacher/TeacherSubmissionDetail.jsx
+++ Frontend/src/pages/Teacher/TeacherSubmissionDetail.jsx
@@ -16,7 +16,7 @@ export default function TeacherSubmissionDetail() {
       try {
         const token = localStorage.getItem('token');
 
-        const response = await fetch(`http://localhost:5000/api/teacher/submissions/${id}/detail`, {
+        const response = await fetch(`https://wed-edu.onrender.com/api/teacher/submissions/${id}/detail`, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`
